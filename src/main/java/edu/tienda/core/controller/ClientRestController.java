82a1434eeef9df8b77f706adbd332687007da9c3
LeoSuarezO <leonardo901.ls2@gmail.com>
2024-01-19 15:39:05 -0500

    Manejo de principales excepciones

--- src/main/java/edu/tienda/core/controller/ClientRestController.java
+++ src/main/java/edu/tienda/core/controller/ClientRestController.java
@@ -1,8 +1,13 @@
 package edu.tienda.core.controller;
 
 import edu.tienda.core.domain.Client;
+import edu.tienda.core.excceptions.BadRequestException;
+import edu.tienda.core.excceptions.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,38 +23,49 @@ public class ClientRestController {
     );
 
     @GetMapping
-    public List<Client> getClients() {
-        return clients;
+    public ResponseEntity<?> getClients() {
+        return ResponseEntity.ok(clients);
     }
 
     @GetMapping("/{userName}")
-    public Client getClient(@PathVariable String userName) {
+    public ResponseEntity<?> getClient(@PathVariable String userName) {
+        if(userName.length() <=3){
+            throw new BadRequestException("El nombre de usuario debe contener mas de 3 caracteres.");
+        }
         return clients.stream().
                 filter(client -> client.getUserName().equalsIgnoreCase(userName)).
-                findFirst().orElseThrow();
+                findFirst().
+                map(ResponseEntity::ok).
+                orElseThrow(() -> new ResourceNotFoundException("Cliente "+userName+" no encontrado"));
     }
 
     @PostMapping
-    public Client insertClient(@RequestBody Client client) {
+    public ResponseEntity<?> insertClient(@RequestBody Client client) {
         clients.add(client);
-        return client;
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{username}")
+                .buildAndExpand(client.getUserName())
+                .toUri();
+        return ResponseEntity.created(location).body(client);
     }
 
     @PutMapping
-    public Client modifyClient(@RequestBody Client client) {
+    public ResponseEntity<?> modifyClient(@RequestBody Client client) {
         Client clientTemp = clients.stream().filter(client1 -> client1.getUserName().equalsIgnoreCase(client.getUserName())).
                 findFirst().orElseThrow();
         clientTemp.setPassword(client.getPassword());
         clientTemp.setName(client.getName());
-        return clientTemp;
+        return ResponseEntity.ok(clientTemp);
     }
 
     @DeleteMapping("/{userName}")
-    public void deleteClient(@PathVariable String userName){
+    public ResponseEntity deleteClient(@PathVariable String userName){
         Client clientTemp = clients.stream().
                 filter(client -> client.getUserName().equalsIgnoreCase(userName)).
                 findFirst().orElseThrow();
         clients.remove(clientTemp);
+        return ResponseEntity.noContent().build();
     }
 
 }
